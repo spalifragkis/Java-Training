aff29f023be1fdc1682acf52e809b77c80d6a4b0
spalifragkis <spalifragkis@gmail.com>
2020-01-28 01:04:19 +0200

    Update Bank_Account.java

--- src/sa/Bank_Account.java
+++ src/sa/Bank_Account.java
@@ -15,6 +15,7 @@ public class Bank_Account {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hey Dude");
+		System.out.println("Github");
 	}
 
 }
